147632802724cce30fc5f204aa7118e3b3a18d88
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-12-21 15:28:47 +0000

    feat: implement fetching data for pull requests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::sync::Arc;
+
 use anyhow::{anyhow, bail, Context, Result};
-use serde::Deserialize;
+use reqwest::{get, header::USER_AGENT};
+use serde::{Deserialize, Serialize};
+use tokio::{sync::Semaphore, task::JoinSet};
 
 fn git<I, S>(args: I) -> Result<String>
 where
@@ -25,24 +29,96 @@ where
     }
 }
 
+static CONFIG_FILE: &str = ".gitpatcher.toml";
+
 #[derive(Deserialize)]
+#[serde(rename_all = "kebab-case")]
 struct Configuration {
     repo: String,
+    remote_branch: String,
     local_branch: String,
     pull_requests: Vec<String>,
 }
 
-fn main() -> Result<()> {
+#[derive(Serialize, Deserialize, Debug)]
+struct GitHubResponse {
+    head: Head,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Head {
+    repo: Repo,
+    r#ref: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Repo {
+    clone_url: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     if git(["rev-parse", "--is-inside-work-tree"]).is_err() {
         bail!("Not in a git repository");
     }
 
     let config_raw = std::env::current_dir()
-        .map(|z| z.join(".gitpatcher.toml"))
+        .map(|z| z.join(CONFIG_FILE))
         .and_then(std::fs::read_to_string)
-        .context("Could not find `.gitpatcher.toml` configuration file")?;
+        .context(format!("Could not find `{CONFIG_FILE}` configuration file"))?;
 
-    let config = toml::from_str::<Configuration>(&config_raw)?;
+    let config = toml::from_str::<Configuration>(&config_raw).context(format!(
+        "Could not parse `{CONFIG_FILE}` configuration file"
+    ))?;
+
+    let client = Arc::new(reqwest::Client::new());
+
+    let mut set = JoinSet::new();
+
+    let requests = config.pull_requests.iter().map(|pr| {
+        client
+            .clone()
+            .get(format!(
+                "https://api.github.com/repos/{}/pulls/{pr}",
+                config.repo
+            ))
+            .header(USER_AGENT, "gitpatcher")
+            .send()
+    });
+
+    for fut in requests {
+        set.spawn(fut);
+    }
+
+    while let Some(res) = set.join_next().await {
+        let out = res??.text().await?;
+        let response: GitHubResponse = serde_json::from_str(&out).unwrap();
+        dbg!(response);
+    }
+
+    // const TASKS_LIMIT: usize = 3;
+
+    // let semaphore = Arc::new(Semaphore::new(TASKS_LIMIT));
+
+    // for _ in 0..5 {
+    //     let permit = semaphore.clone().acquire_owned().await.unwrap();
+    //     tokio::spawn(async move {
+    //         let resp = client
+    //             .clone()
+    //             .get(format!(
+    //                 "https://api.github.com/repos/helix-editor/helix/pulls/12309"
+    //             ))
+    //             .header(USER_AGENT, "gitpatcher")
+    //             .send()
+    //             .await?
+    //             .text()
+    //             .await?;
+
+    //         drop(permit);
+    //     });
+    // }
+
+    // semaphore.acquire_many(TASKS_LIMIT as u32).await.unwrap();
 
     Ok(())
 }
