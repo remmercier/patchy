64885156241e37f9b92fb643fcbf15b063aa63b8
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-12-22 09:49:51 +0000

    feat: change color of branch

--- src/main.rs
+++ src/main.rs
@@ -186,7 +186,7 @@ async fn main() -> Result<()> {
         .with_prompt(format!(
             "\n    {} Overwrite branch {}? This is irreversible.",
             "»".black(),
-            config.local_branch.blue()
+            config.local_branch.cyan()
         ))
         .interact()
         .unwrap();
@@ -210,7 +210,7 @@ async fn main() -> Result<()> {
         let command = format!("\n{INDENT}{}\n", command.magenta(),);
         println!(
             "\n{INDENT}You can still manually overwrite {} with the following command:\n{command}\n",
-            config.local_branch.blue().italic(),
+            config.local_branch.cyan().italic(),
         )
     }
 
