03ae5a102a25c0d7a12678eae21ab1ef5e804131
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-12-22 01:26:36 +0000

    feat: update styles for last_commit_message when patch is applied

--- src/main.rs
+++ src/main.rs
@@ -167,7 +167,7 @@ async fn main() -> Result<()> {
                     &format!("{CONFIG_ROOT}/{file_name}"),
                 ])
                 .context(format!("Could not apply patch {file_name}, skipping"))?;
-                let last_commit_message = git(&["log", "-1", "--format=%B"])?;
+                let last_commit_message = git(&["log", "-1", "--format='%B'"])?;
                 let success_message = success!(
                     "Applied patch {file_name} {}",
                     last_commit_message.blue().italic()
