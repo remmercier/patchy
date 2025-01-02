6b6d4c015f6ae8382f3061356a772b3915efed7e
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-01-02 02:49:59 +0000

    build: for nixOS

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A tool which makes it easy to declaratively manage personal forks by automatically merging pull requests";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = nixpkgs.legacyPackages;
+    in
+    {
+      packages = forAllSystems (system: {
+        default = pkgsFor.${system}.callPackage ./. { };
+      });
+      devShells = forAllSystems (system: {
+        default = pkgsFor.${system}.callPackage ./shell.nix { };
+      });
+    };
+}
