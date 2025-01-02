a4d42a2c006a886d2b2cc826359c7a9647cc532a
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-01-02 03:50:53 +0000

    feat: rename package to patchy-bin

--- default.nix
+++ default.nix
@@ -2,11 +2,11 @@
   pkgs ? import <nixpkgs> { },
 }:
 let
-  manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+  manifest = pkgs.lib.importTOML ./Cargo.toml;
 in
 pkgs.rustPlatform.buildRustPackage {
-  pname = manifest.name;
-  version = manifest.version;
+  pname = manifest.lib.name;
+  version = manifest.package.version;
 
   cargoLock.lockFile = ./Cargo.lock;
 
